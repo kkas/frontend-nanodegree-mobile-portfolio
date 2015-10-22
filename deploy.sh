6acd44249e76bade5779c790f789244acf7c3142
kkas <kentakikui@gmail.com>
2015-10-22 14:11:27 +0900

    chore: add deploy script to deploy only a specific directory to the gh-pages branch

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# This script is borrowed from the following gist.
+# https://gist.github.com/cobyism/4730490
+#!/bin/sh
+if [ -z "$1" ]
+then
+  echo "Which folder do you want to deploy to GitHub Pages?"
+  exit 1
+fi
+git subtree push --prefix $1 origin gh-pages
+
