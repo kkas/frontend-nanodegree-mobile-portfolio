c2d322f5687a7c31ff70c49323f0664712167922
kkas <kentakikui@gmail.com>
2015-10-28 16:51:51 +0900

    fix: fix Shebang

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 # This script is borrowed from the following gist.
 # https://gist.github.com/cobyism/4730490
-#!/bin/sh
 if [ -z "$1" ]
 then
   echo "Which folder do you want to deploy to GitHub Pages?"
